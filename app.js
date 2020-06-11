f3d46de9557fec47013208fe6aaffafb29435b46
SilvenLEAF <silvenleaf@gmail.com>
2020-06-11 17:29:06 +0530

    Basic Upload Setup

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 
+
+
+/* ---------------------------
+.            upload
+--------------------------- */
+//set up storage engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: (req, file, cb)=>{
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//init upload
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1000000},
+    fileFilter: (req, file, cb)=>{
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+/* --------checkFileType-------- */
+function checkFileType(file, cb) {
+    //allowed ext
+    const fileTypes = /jpeg|jpg|png|gif/;
+    //check ext
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    //check mime
+    const mimetype = fileTypes.test(file.mimetype);
+
+
+    if(mimetype && extname){
+        return cb(null, true);
+    }else{
+        cb('Error: Images ONLY');
+    }
+}
+//--------------------------------
 
 
 //firing express app
