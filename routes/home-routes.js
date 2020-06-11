5d518a6b938227960e9d80713485bfcf1fa796eb
SilvenLEAF <silvenleaf@gmail.com>
2020-06-11 17:52:25 +0530

    Upload works. Upload POST handled. Also **app.use('/uploadsURL', express.static('../public/uploads'))**

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,4 +1,52 @@
 const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
+
+
+
+/* ---------------------------
+.            upload
+--------------------------- */
+//set up storage engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: (req, file, cb)=>{
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//init upload
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1000000},
+    fileFilter: (req, file, cb)=>{
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+/* --------checkFileType-------- */
+function checkFileType(file, cb) {
+    //allowed ext
+    const fileTypes = /jpeg|jpg|png|gif/;
+    //check ext
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    //check mime
+    const mimetype = fileTypes.test(file.mimetype);
+
+
+    if(mimetype && extname){
+        return cb(null, true);
+    }else{
+        cb('Error: Images ONLY');
+    }
+}
+//--------------------------------
+
+
+
+/* ---------------------------
+.            routes
+--------------------------- */
 
 router.get('/', (req, res, next)=>{
   try{
@@ -7,6 +55,23 @@ router.get('/', (req, res, next)=>{
       next();
   }
     
+});
+
+router.post('/upload', (req, res, next)=>{
+    upload(req, res, (err)=>{
+        if(err){
+            res.render('index', {msg: err});
+        }else{
+            if(req.file == undefined){
+                res.render('index', {msg: 'No file Selected'});
+            }else{
+                res.render('index', {
+                    msg: 'Image uploaded',
+                    file: `/uploadsURL/${req.file.filename}`
+                })
+            }
+        }
+    })
 })
 
 
